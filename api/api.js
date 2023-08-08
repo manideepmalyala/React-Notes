daf58322e59837f08e8e8d13ec9d7feae7d94ddc
Tapan Meena <tapanme@maqsoftware.com>
2023-08-08 23:47:22 +0530

    Implemented firebase user data retrieval

--- api/api.js
+++ api/api.js
@@ -1,31 +1,33 @@
 import { db } from "./firebase";
 import crypto from "crypto-js";
-import { collection, getDoc, query, where } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 export function getCollection(collectionName) {
-    return collection(db, collectionName);
+  return collection(db, collectionName);
 }
 
-export function convertStringToHash(string){
-    return crypto.SHA256(string).toString();
+export function convertStringToHash(string) {
+  return crypto.SHA256(string).toString();
 }
 
-export function checkLoginExists(username, password){
-    const usersCollection = getCollection("users");
-    const hashed_password = convertStringToHash(password);
-    const user = query(usersCollection,where("username", "==", "tapan")) //.where("password", "==", hashed_password)
-    console.log(user)
-    if(user.exists){
-        return true;
-    }
-    return false;
+export async function checkLoginExists(email, password) {
+  const usersCollection = getCollection("users");
+  const hashed_password = convertStringToHash(password);
+  const q = query(
+    usersCollection,
+    where("email", "==", email),
+    where("password", "==", password) // temp solution until hashed password is implemented
+    // where("password", "==", hashed_password)
+  );
+
+  const querySnapshot = await getDocs(q);
+  return !querySnapshot.empty;
 }
 
-export function checkUserExists(username){
-    const usersCollection = getCollection("users");
-    const user = query(usersCollection,where("username", "==", username))
-    if(user.exists){
-        return true;
-    }
-    return false;
+export async function checkUserExists(email) {
+  const usersCollection = getCollection("users");
+  const q = query(usersCollection, where("email", "==", email));
+
+  const querySnapshot = await getDocs(q);
+  return !querySnapshot.empty;
 }
--- components/Login.jsx
+++ components/Login.jsx
@@ -1,19 +1,40 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { checkLoginExists } from "../api/api";
 
-export default function Login({signUp,isSigningUp}){
-    
-    return(
-        <div className="login">
-            <h1>{isSigningUp?"Sign up":"Log in"}</h1>
-            <form className="login--form">
-                <input type="text" placeholder="Username" />
-                <input type="password" placeholder="Password"/>
-                {isSigningUp && <input type="password" placeholder="Confirm Password"/>}
-                <button>{isSigningUp?"Sign up":"Log in"}</button>
-            </form>
-            <p>{isSigningUp?"Already have an account?":"Don't have an account?"} <a onClick={signUp} href="#">{isSigningUp?"Log in":"Sign up"}</a></p>
-            
-        </div>
-        )
+export default function Login({ signUp, isSigningUp }) {
+  const SubmitInfo = async (e) => {
+    e.preventDefault();
+    console.log("submitLogin");
+    const userResult = await checkLoginExists(e.target.username.value, e.target.password.value);
+    alert(userResult? "Login successful" : "Login failed");
+  };
+  return (
+    <div className="login" onSubmit={SubmitInfo}>
+      <h1>{isSigningUp ? "Sign up" : "Log in"}</h1>
+      <form className="login--form">
+        <input name="username" type="text" placeholder="Username" required />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+        />
+        {isSigningUp && (
+          <input
+            name="confirm-password"
+            type="password"
+            placeholder="Confirm Password"
+            required
+          />
+        )}
+        <button>{isSigningUp ? "Sign up" : "Log in"}</button>
+      </form>
+      <p>
+        {isSigningUp ? "Already have an account?" : "Don't have an account?"}{" "}
+        <a onClick={signUp} href="#">
+          {isSigningUp ? "Log in" : "Sign up"}
+        </a>
+      </p>
+    </div>
+  );
 }
