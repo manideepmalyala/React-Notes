a53dc7f02427c7c09af04b2a41181ff543ffd494
Tapan Meena <tapanme@maqsoftware.com>
2023-08-08 13:13:13 +0530

    Added app

--- App.jsx
+++ App.jsx
@@ -1,127 +1,120 @@
-import React from "react"
-import Sidebar from "./components/Sidebar"
-import Editor from "./components/Editor"
-import Split from "react-split"
-import { onSnapshot, addDoc, deleteDoc, doc, setDoc } from "firebase/firestore"
-import { notesCollection, db } from "./firebase"
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import Editor from "./components/Editor";
+import Split from "react-split";
+import { onSnapshot, addDoc, deleteDoc, doc, setDoc } from "firebase/firestore";
+import { notesCollection, db } from "./firebase";
 
 export default function App() {
-    const [notes, setNotes] = React.useState([])
-    const [currentNoteId, setCurrentNoteId] = React.useState("")
-    const [tempNoteText, setTempNoteText] = React.useState("")
-    const [selectedTab, setSelectedTab] = React.useState("write")
-    const [theme, setTheme] = React.useState(true)
-    const themeStyles ={
-        backgroundColor: theme ? "#1a1a1a" : "#fff",
-        color: theme ? "#fff" : "#1a1a1a",
+  const [notes, setNotes] = React.useState([]);
+  const [currentNoteId, setCurrentNoteId] = React.useState("");
+  const [tempNoteText, setTempNoteText] = React.useState("");
+  const [selectedTab, setSelectedTab] = React.useState("write");
+  const [theme, setTheme] = React.useState(true);
+  const themeStyles = {
+    backgroundColor: theme ? "#1a1a1a" : "#fff",
+    color: theme ? "#fff" : "#1a1a1a",
+  };
+
+  const sortedNotes = notes.sort((a, b) => b.updatedAt - a.updatedAt);
+
+  const currentNote =
+    notes.find((note) => note.id === currentNoteId) || notes[0];
+
+  React.useEffect(() => {
+    const unsubscribe = onSnapshot(notesCollection, function (onSnapshot) {
+      const updatedNotes = onSnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setNotes(updatedNotes);
+    });
+    return unsubscribe;
+  }, []);
+
+  React.useEffect(() => {
+    if (!currentNoteId) {
+      setCurrentNoteId(notes[0]?.id);
     }
-
-
-    const sortedNotes = notes.sort((a, b) => b.updatedAt - a.updatedAt)
-
-    const currentNote =
-        notes.find(note => note.id === currentNoteId)
-        || notes[0]
-
-    React.useEffect(() => {
-        const unsubscribe = onSnapshot(notesCollection, function (onSnapshot) {
-            const updatedNotes = onSnapshot.docs.map(doc => (
-                {
-                    ...doc.data(),
-                    id: doc.id
-                }))
-            setNotes(updatedNotes)
-        })
-        return unsubscribe
-    }, [])
-
-    React.useEffect(() => {
-        if (!currentNoteId) {
-            setCurrentNoteId(notes[0]?.id)
-        }
-    }, [notes])
-    React.useEffect(() => {
-        if (currentNote) {
-            setTempNoteText(currentNote.body)
-        }
-    }, [currentNote])
-    React.useEffect(() => {
-        const timeoutId = setTimeout(() => {
-            if (tempNoteText !== currentNote.body)
-                updateNote(tempNoteText)
-        }, 500)
-        return () => clearTimeout(timeoutId)
-    }, [tempNoteText])
-
-    async function createNewNote() {
-        const newNote = {
-            body: "# Type your markdown note's title here",
-            createdAt: Date.now(),
-            updatedAt: Date.now()
-        }
-        const newNoteRef = await addDoc(notesCollection, newNote)
-        setCurrentNoteId(newNoteRef.id)
-        setSelectedTab("write")
+  }, [notes]);
+  React.useEffect(() => {
+    if (currentNote) {
+      setTempNoteText(currentNote.body);
     }
+  }, [currentNote]);
+  React.useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      if (tempNoteText !== currentNote.body) updateNote(tempNoteText);
+    }, 500);
+    return () => clearTimeout(timeoutId);
+  }, [tempNoteText]);
 
-    async function updateNote(text) {
-        setTempNoteText(text)
-        const noteRef = doc(db, "notes", currentNoteId)
-        await setDoc(noteRef, { body: text, updatedAt: Date.now() }, { merge: true })
-    }
+  React.useEffect(() => {
+    // if existing note is selected then selectedTab should be "preview" by default
+    setSelectedTab("preview");
+  },[currentNoteId]);
 
-    async function deleteNote(noteId) {
-        const noteRef = doc(db, "notes", noteId)
-        await deleteDoc(noteRef)
+  async function createNewNote() {
+    const newNote = {
+      body: "# Type your markdown note's title here",
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    };
+    const newNoteRef = await addDoc(notesCollection, newNote);
+    setCurrentNoteId(newNoteRef.id);
+    setSelectedTab("write");
+  }
 
-    }
-    function handleTabChange() {
-        setSelectedTab(selectedTab === "write" ? "preview" : "write")
-    }
-    function toggleTheme() {
-        setTheme(prevTheme => !prevTheme)
-    }
+  async function updateNote(text) {
+    setTempNoteText(text);
+    const noteRef = doc(db, "notes", currentNoteId);
+    await setDoc(
+      noteRef,
+      { body: text, updatedAt: Date.now() },
+      { merge: true }
+    );
+  }
 
-    return (
-        <main style={themeStyles}>
-            {
-                notes.length > 0
-                    ?
-                    <Split
-                        sizes={[30, 70]}
-                        direction="horizontal"
-                        className="split"
-                    >
-                        <Sidebar
-                            notes={sortedNotes}
-                            currentNote={currentNote}
-                            setCurrentNoteId={setCurrentNoteId}
-                            newNote={createNewNote}
-                            deleteNote={deleteNote}
-                            handleTabChange={handleTabChange}
-                            selectedTab={selectedTab}
-                            toggleTheme={toggleTheme}
-                            theme={theme}
-                        />
-                        <Editor
-                            tempNoteText={tempNoteText}
-                            setTempNoteText={setTempNoteText}
-                            selectedTab={selectedTab}
-                        />
+  async function deleteNote(noteId) {
+    const noteRef = doc(db, "notes", noteId);
+    await deleteDoc(noteRef);
+  }
+  function handleTabChange() {
+    setSelectedTab(selectedTab === "write" ? "preview" : "write");
+  }
+  function toggleTheme() {
+    setTheme((prevTheme) => !prevTheme);
+  }
 
-                    </Split>
-                    :
-                    <div className="no-notes">
-                        <h1>You have no notes</h1>
-                        <button
-                            className="first-note"
-                            onClick={createNewNote}
-                        >
-                            Create one now
-                        </button>
-                    </div>
-
-            }
-        </main>
-    )
+  return (
+    <main style={themeStyles}>
+      {notes.length > 0 ? (
+        <Split sizes={[30, 70]} direction="horizontal" className="split">
+          <Sidebar
+            notes={sortedNotes}
+            currentNote={currentNote}
+            setCurrentNoteId={setCurrentNoteId}
+            newNote={createNewNote}
+            deleteNote={deleteNote}
+            handleTabChange={handleTabChange}
+            selectedTab={selectedTab}
+            toggleTheme={toggleTheme}
+            theme={theme}
+          />
+          <Editor
+            tempNoteText={tempNoteText}
+            setTempNoteText={setTempNoteText}
+            selectedTab={selectedTab}
+          />
+        </Split>
+      ) : (
+        <div className="no-notes">
+          <h1>You have no notes</h1>
+          <button className="first-note" onClick={createNewNote}>
+            Create one now
+          </button>
+        </div>
+      )}
+    </main>
+  );
 }
