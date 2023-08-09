81ffa75615f073d9aa8f747d09e1ec14245050c1
v-tapanmeena <126665567+v-tapanmeena@users.noreply.github.com>
2023-08-09 16:54:41 +0530

    Manideep/login page (#8)
    * added smooth transition
    
    * Added new usercollection and loginpage
    
    * added api
    
    * added firebase
    
    * added package
    
    * adde userExists function
    
    * Added signup functionality
    
    * Updated readme
    
    * Implemented firebase user data retrieval
    
    * fixed signup
    
    * Refactored the code
    
    * added function
    
    * Login
    
    * added login functionality
    
    * added login
    
    * added alert
    
    * Added unique Id for notes
    
    ---------
    
    Co-authored-by: Manideep <manideepm20@gmail.com>
    Co-authored-by: Tapan Meena <tapanme@maqsoftware.com>

---
+++ api/api.js
@@ -0,0 +1,83 @@
+import { db } from "./firebase";
+import crypto from "crypto-js";
+import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
+
+/**
+ * Returns a reference to the Firestore collection with the given name.
+ * @param {string} collectionName - The name of the collection to retrieve.
+ * @returns {CollectionReference} A reference to the Firestore collection.
+ */
+export function getCollection(collectionName) {
+  return collection(db, collectionName);
+}
+
+/**
+ * Returns a hashed version of the given string using the SHA256 algorithm.
+ * @param {string} string - The string to hash.
+ * @returns {string} The hashed version of the string.
+ */
+export function convertStringToHash(string) {
+  return crypto.SHA256(string).toString();
+}
+
+/**
+ * Checks if a user with the given email and password exists in the database.
+ * @param {string} email - The email of the user to check.
+ * @param {string} password - The password of the user to check.
+ * @returns {Promise<boolean>} A Promise that resolves to true if the user exists, false otherwise.
+ */
+export async function checkLoginExists(email, password) {
+  const usersCollection = getCollection("users");
+  const hashed_password = convertStringToHash(password);
+  const q = query(
+    usersCollection,
+    where("email", "==", email),
+    where("password", "==", hashed_password)
+  );
+
+  const querySnapshot = await getDocs(q);
+  return !querySnapshot.empty;
+}
+
+/**
+ * Checks if a user with the given email exists in the database.
+ * @param {string} email - The email of the user to check.
+ * @returns {Promise<boolean>} A Promise that resolves to true if the user exists, false otherwise.
+ */
+export async function checkUserExists(email) {
+  const usersCollection = getCollection("users");
+  const q = query(usersCollection, where("email", "==", email));
+
+  const querySnapshot = await getDocs(q);
+  return !querySnapshot.empty;
+}
+
+/**
+ * Creates a new user in the database with the given email and password.
+ * @param {{email: string, password: string}} userdetails - An object containing the email and password of the user to create.
+ * @returns {Promise<DocumentReference>} A Promise that resolves to the user's credentials if the user is created successfully.
+ * @throws An error if there is a problem creating the user.
+ */
+export async function createUser(userdetails) {
+  const usersCollection = getCollection("users");
+  const hashed_password = convertStringToHash(userdetails.password);
+
+  try {
+    const userCredential = await addDoc(usersCollection, {
+      email: userdetails.email,
+      password: hashed_password,
+      createdOn: Date.now(),
+    });
+    alert("User created successfully");
+    return userCredential;
+  } catch (ex) {
+    alert("Error creating user", ex);
+  }
+}
+
+export async function getuserDocumentID(email){
+  const usersCollection = getCollection("users");
+  const q = query(usersCollection, where("email", "==",email));
+  const querySnapshot = await getDocs(q);
+  return querySnapshot.docs[0].id;
+}
