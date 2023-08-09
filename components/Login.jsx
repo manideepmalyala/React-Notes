74458426ddb38d87557dc18dcd2c2ceb8f12a933
Tapan Meena <tapanme@maqsoftware.com>
2023-08-09 14:28:42 +0530

    added alert

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { checkLoginExists, checkUserExists, createUser } from "../api/api";
+import { Alert } from '@mui/material';
 
 export default function Login({ setIsLoggedIn }) {
   const [userdetails, setUserDetails] = React.useState({
@@ -7,6 +8,8 @@ export default function Login({ setIsLoggedIn }) {
     password: "",
     confirmPassword: ""
   });
+
+  const [loginError, setLoginError] = React.useState(false);
 
   const [user, setUser] = React.useState();
   const [login, setLogin] = React.useState(true);
@@ -39,54 +42,57 @@ export default function Login({ setIsLoggedIn }) {
   }
   function IsLoggingIn() {
     setLogin(!login);
+    setLoginError(false);
   }
 
   async function checkLoginIsValid(event) {
     event.preventDefault();
     const validLogin = await checkLoginExists(userdetails.email, userdetails.password);
     if (validLogin) {
-      alert("Login successful");
       setIsLoggedIn(true);
     }
     else {
-      alert("Invalid login details");
+      setLoginError(true);
     }
   }
 
   return (
-    <div className="login">
-      <form className="login--form">
-        <input
-          type="email"
-          placeholder="Email address"
-          name="email"
-          onChange={change}
-          value={userdetails.email}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          name="password"
-          onChange={change}
-          value={userdetails.password}
-        />
-        {!login && <input
-          type="password"
-          placeholder="Confirm password"
-          name="confirmPassword"
-          onChange={change}
-          value={userdetails.confirmPassword}
-        />}
-        {login ? <button onClick={(event) => checkLoginIsValid(event)}>Log in</button> :
-          <button onClick={(event) => UserCreation(event)}>Sign up</button>}
-
-      </form>
-      <div className="form--footer">
-        {!login ? <p>
-          Already have an account? <a href="#" onClick={IsLoggingIn}>Log in</a>
-        </p> :
-          <p>Don't have an account? <a href="#" onClick={IsLoggingIn}>Sign up</a></p>}
+    <>
+      <div className="login">
+        <form className="login--form">
+          <input
+            type="email"
+            placeholder="Email address"
+            name="email"
+            onChange={change}
+            value={userdetails.email}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={change}
+            value={userdetails.password}
+          />
+          {!login && <input
+            type="password"
+            placeholder="Confirm password"
+            name="confirmPassword"
+            onChange={change}
+            value={userdetails.confirmPassword}
+          />}
+          {login ? <button onClick={(event) => checkLoginIsValid(event)}>Log in</button> :
+            <button onClick={(event) => UserCreation(event)}>Sign up</button>}
+          <br />
+          {loginError && <Alert variant="filled" severity="error" >Invalid login details</Alert>}
+        </form>
+        <div className="form--footer">
+          {!login ? <p>
+            Already have an account? <a href="#" onClick={IsLoggingIn}>Log in</a>
+          </p> :
+            <p>Don't have an account? <a href="#" onClick={IsLoggingIn}>Sign up</a></p>}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
